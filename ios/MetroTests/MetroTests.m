746f4deb181d848c349ada26e079992c6ef96dfc
ayesamespejo <Miguel Espejo>
2025-08-19 11:25:04 -0400

    Actulizar nombre app "Metro"

--- ios/MetroTests/MetroTests.m
+++ ios/MetroTests/MetroTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface metroappTests : XCTestCase
+@interface MetroTests : XCTestCase
 
 @end
 
-@implementation metroappTests
+@implementation MetroTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
